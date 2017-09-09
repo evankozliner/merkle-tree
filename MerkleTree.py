428c148428ab9e3e92d3fde146992ad020ccdb79
Evan Kozliner <evankozliner@gmail.com>
2017-09-09 12:45:44 -0700

    initial draft of tree

---
+++ MerkleTree.py
@@ -0,0 +1,209 @@
+import os
+import math
+import hashlib
+
+""" TODO
+    1. File implementation example
+"""
+
+
+class MerkleTree:
+    class Node:
+        def __init__(self, mom, dad, hash_val):
+            self.mom = mom
+            self.dad = dad
+            if mom == None:
+                self._height = 0
+            else:
+                self._height = mom.height + 1
+
+            self.hash = hash_val
+            self.child = None
+    
+        @property
+        def height(self):
+            return self._height
+    
+        @height.setter
+        def height(self,height):
+            if height < 0:
+                raise Exception("Attempt to set height < 0.")
+            self._height = height
+
+        def get_parent_by_spouse(self, spouse_hash):
+            if spouse_hash == self.mom.hash:
+                return self.dad
+            return self.mom
+
+    def __init__(self, items):
+        if len(items) <= 0:
+            raise Exception("items must contain at least 1" + \
+                    "element for a valid merkle tree.")
+        self.is_built = True
+        self.root_hash = None
+        self.node_table = {}
+        self.max_height = math.ceil(math.log(len(items), 2))
+        # TODO leaves should be internal
+        self.leaves = map(self._leafify, map(self._md5sum, items))
+        self.build_tree()
+
+    def _leafify(self, data):
+        leaf = self.Node(None, None, data)
+        leaf.height = 0
+        return leaf
+
+    def _get_authentication_path_by_hash(self, hash_):
+        """ Returns an authentication path as a list in order from the top
+            to the bottom of the tree (assumes preconditions have been checked).
+        """
+        path = []
+        while hash_ != self.root_hash:
+            node = self.node_table[hash_]
+            child = node.child
+            spouse = child.get_parent_by_spouse(hash_)
+            path.append(spouse.hash)
+            hash_ = child.hash
+
+        path.append(hash_)
+        path.reverse()
+        return path
+    
+    def _md5sum(self, data):
+        """ Returns an md5 hash of data. 
+            If data is a file it is expected to contain its full path.
+        """
+        data = str(data)
+        m = hashlib.md5()
+        if os.path.isfile(data):
+            try:   
+                f = file(fn, 'rb')
+            except:
+                return 'ERROR: unable to open %s' % fn
+            while True:
+                d = f.read(8096)
+                if not d:
+                    break
+                m.update(d)
+            f.close()
+        # Otherwise it could be either 1) a directory 2) miscellaneous data (like json)
+        else:
+            m.update(data)
+        return m.hexdigest()
+
+    def _audit(self, questioned_hash, proof_hashes):
+        """ Tests if questioned_hash is a member of the merkle tree by
+            hashing it with its sibling until the root hash is reached. 
+        """
+        proof_hash = proof_hashes.pop()
+
+        if not proof_hash in self.node_table.keys():
+            return False
+
+        sibling = self.node_table[proof_hash]
+        child = sibling.child
+
+        # Because the order in which the hashes are concatenated matters,
+        # we must test to see if questioned_hash is the "mother" or "father"
+        # of its child (the hash is always build as mother + father).
+        if child.mom.hash == questioned_hash:
+            actual_hash = self._md5sum(questioned_hash + sibling.hash)
+        elif child.dad.hash == questioned_hash:
+            actual_hash = self._md5sum(sibling.hash + questioned_hash)
+        else:
+            return False
+
+        if actual_hash != child.hash:
+            return False
+        if actual_hash == self.root_hash:
+            return True
+
+        return self._audit(actual_hash, proof_hashes)
+
+    # TODO can this be consolidated into the earlier method?
+    def _handle_solo_node_case(self,):
+        """ If only one item is provided, neither case will work. """
+        if len(self.leaves) == 1:
+            solo_node = self.leaves.pop()
+            self.root_hash = solo_node.hash
+            self.node_table[solo_node.hash] = solo_node
+
+    def _get_leaf_hashes(self):
+        return [node.hash for node in self.node_table.values() if node.mom == None]
+
+    # TODO break into sub methods?
+    def build_tree(self):
+        """ Builds a merkle tree by adding leaves one at a time to a stack,
+            and combining leaves in the stack when they are of the same height.
+            Expected items to be an array of type Node.
+            Also constructs node_table, a dict containing hashes that map to 
+            individual nodes for auditing purposes.
+        """
+        stack = []
+        self._handle_solo_node_case()
+        while self.root_hash == None:
+            if len(stack) >= 2 and stack[-1].height == stack[-2].height:
+                # TODO clean this up
+                mom = stack.pop()
+                dad = stack.pop()
+                child_hash = self._md5sum(mom.hash + dad.hash)
+                child = self.Node(mom, dad, child_hash)
+                self.node_table[child_hash] = child
+                mom.child = child
+                dad.child = child
+
+                if child.height == self.max_height:
+                    self.root_hash = child.hash
+
+                stack.append(child)
+            elif len(self.leaves) > 0:
+                leaf = self.leaves.pop()
+                self.node_table[leaf.hash] = leaf
+                stack.append(leaf)
+            # Handle case where last 2 nodes do not match in height by "graduating"
+            # last node
+            else:
+                stack[-1].height += 1
+        self.is_built = True
+
+    def audit(self, data, proof_hashes):
+        """ Returns a boolean testing if a data (a file or object)
+            is contained in the merkle tree. 
+
+            proof_hashes are the nodes to hash the hash of data with 
+            in order from the bottom of the tree to the second-to-last
+            level. len(proof_hashes) is expected to be the height of the
+            tree, ceil(log2(n)), as one node is needed for proof per layer.
+
+            If the tree has not been built, returns False for any data.
+        """
+        if self.root_hash == None:
+            return False
+
+        # A one element tree does not make much sense, but in case one exists
+        # this simple test should be sufficient for validity
+        if self.max_height == 0 and proof_hashes[0] == self.root_hash:
+            return True
+        if self.max_height == 0 and proof_hashes[0] != self.root_hash:
+            return False
+
+        return self._audit(self._md5sum(data), proof_hashes)
+    
+    def get_authentication_path(self, item):
+        """ Returns an authentication path for an item (not hashed) in 
+            the Merkle tree as a list in order from the top of the tree
+            to the bottom.
+        """
+        if not self.is_built:
+            raise Exception("The Merkle Tree must be built before an \
+                    authentication path is found.")
+
+        hash_ = self._md5sum(item)
+
+        if not hash_ in self._get_leaf_hashes():
+            raise Exception("The requested item is not in the merkle tree.")
+
+        return self._get_authentication_path_by_hash(hash_)
+
+
+
+        
---
+++ MerkleTreeTest.py
@@ -0,0 +1,86 @@
+import unittest
+import hashlib
+from MerkleTree import MerkleTree
+
+ONE_ELEMENT_DATA = "text"
+TWO_ELEMENT_DATA = ["one", "two"]
+THREE_ELEMENT_DATA = ["one", "two", "three"]
+
+def md5sum(data):
+    data = str(data)
+    m = hashlib.md5()
+    m.update(data)
+    return m.hexdigest()
+
+class TestMerkleTree(unittest.TestCase):
+    def test_empty_tree_error(self):
+        with self.assertRaises(Exception) as cm:
+            tree = MerkleTree([])
+
+    def test_one_element_tree(self):
+        tree = MerkleTree([ONE_ELEMENT_DATA])
+        expected_root_hash = md5sum(ONE_ELEMENT_DATA)
+
+        self.assertEqual(expected_root_hash, tree.root_hash)
+        self.assertEqual(0, tree.max_height)
+
+    def test_two_element_tree(self):
+        tree = MerkleTree(TWO_ELEMENT_DATA)
+        expected_root_hash = md5sum(md5sum(TWO_ELEMENT_DATA[0]) + \
+                md5sum(TWO_ELEMENT_DATA[1]))
+        self.assertEqual(expected_root_hash, tree.root_hash)
+        self.assertEqual(1, tree.max_height)
+
+    def test_three_element_tree(self):
+        tree = MerkleTree(THREE_ELEMENT_DATA)
+        expected_root_hash = md5sum(md5sum(THREE_ELEMENT_DATA[0]) + \
+                md5sum(md5sum(THREE_ELEMENT_DATA[1]) + \
+                md5sum(THREE_ELEMENT_DATA[2])))
+        self.assertEqual(expected_root_hash, tree.root_hash)
+        self.assertEqual(2, tree.max_height)
+        self.assertEqual(5, len(tree.node_table.keys()))
+
+    def test_one_element_audit(self):
+        false_data = "not in tree"
+        data_hash = md5sum(ONE_ELEMENT_DATA)
+        data_hash_false = md5sum(false_data)
+        tree = MerkleTree([ONE_ELEMENT_DATA])
+        result_true = tree.audit(ONE_ELEMENT_DATA, [data_hash])
+        result_false = tree.audit(false_data, [data_hash_false])
+
+        self.assertTrue(result_true)
+        self.assertFalse(result_false)
+
+    def test_two_element_audit_valid(self):
+        tree = MerkleTree(TWO_ELEMENT_DATA)
+        result = tree.audit(TWO_ELEMENT_DATA[0], [md5sum(TWO_ELEMENT_DATA[1])])
+        self.assertTrue(result)
+
+    def test_three_element_hanging_node_audit(self):
+        tree = MerkleTree(THREE_ELEMENT_DATA)
+        proof = [md5sum(md5sum(THREE_ELEMENT_DATA[1]) + \
+                md5sum(THREE_ELEMENT_DATA[2]))]
+        result = tree.audit(THREE_ELEMENT_DATA[0], proof)
+        self.assertTrue(result)
+
+    def test_three_element_valid(self):
+        tree = MerkleTree(THREE_ELEMENT_DATA)
+        proof = [ md5sum(THREE_ELEMENT_DATA[0]),md5sum(THREE_ELEMENT_DATA[1]) ]
+        result = tree.audit(THREE_ELEMENT_DATA[2], proof)
+        self.assertTrue(result)
+
+    def test_three_element_invalid(self):
+        return
+
+    def test_three_element_auth_path(self):
+        tree = MerkleTree(THREE_ELEMENT_DATA)
+        result = tree.get_authentication_path("one")
+        sibling_hash = md5sum(md5sum(THREE_ELEMENT_DATA[1]) + \
+                md5sum(THREE_ELEMENT_DATA[2]))
+        self.assertEqual(result, 
+                [md5sum(md5sum(THREE_ELEMENT_DATA[0]) + sibling_hash),
+                 sibling_hash])
+
+if __name__ == "__main__":
+    unittest.main()
+
